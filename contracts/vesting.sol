82eef588a44698ae27bbc771b8cdb126696382f8
pauline-banye <paulinebanye@gmail.com>
2022-03-20 13:51:02 +0100

    check functions

--- contracts/vesting.sol
+++ contracts/vesting.sol
@@ -19,12 +19,12 @@ contract PlynToken is ERC20, Ownable {
         _mint(msg.sender, 1000 * 10 ** decimals());
     }
 
-    // set purchase rate
-    function setRate(uint256 newRate) public onlyOwner {
+    // set purchase rate working
+    function modifyTokenBuyPrice(uint256 newRate) public onlyOwner {
         rate = newRate;
     }
     
-    // purchase tokens
+    // purchase tokens working
     // function buyToken(address buyer, uint256 amount) public payable {
     function buyToken(address buyer) public payable returns (uint256 amount) {
         require (msg.value > 0, "You need money for this transaction");
@@ -33,7 +33,7 @@ contract PlynToken is ERC20, Ownable {
     }
 
 
-    // to verify stakeholder
+    // to verify stakeholder working
     // s = verified
     //  _address = stakeholder_address
     function isStakeholder(address stakeholder_address) public view returns(bool, uint256) {
@@ -47,13 +47,14 @@ contract PlynToken is ERC20, Ownable {
     }
     
 
-    // to add new & remove a stakeholder
+    // to add a stakeholder working
     // _stakeholder = staker
     function addStakeholder(address staker) public {
         (bool _isStakeholder, ) = isStakeholder(staker);
         if(!_isStakeholder) stakeholders.push(staker);
     }
 
+    // to remove a stakeholder working
     function removeStakeholder(address staker) public {
         (bool _isStakeholder, uint256 verified) = isStakeholder(staker);
         if(_isStakeholder){
@@ -62,14 +63,20 @@ contract PlynToken is ERC20, Ownable {
         }
     }
 
+    // view list of stakeholders
+    function viewStakeholder(address staker) public {
+        (bool _isStakeholder,) = isStakeholder(staker);
+        if(!_isStakeholder) stakeholders.push(staker);
+    }
+
     
-    // check amount staked by a stakeholder
+    // check amount staked by a stakeholder working
     function stakeOf(address staker) public view returns (uint256) {
         return stakes[staker];
     }
 
 
-    // check total of all stakes
+    // check total of all stakes working
     function combinedStake() public view returns(uint256) {
         uint256 totalStakes = 0;
         for (
@@ -81,20 +88,28 @@ contract PlynToken is ERC20, Ownable {
         return totalStakes;
     }
 
-    // for stakeholder to add and remove a stake
-    function createStake(uint256 _stake)
-       public
-    {
-        _burn(msg.sender, _stake);
-        if(stakes[msg.sender] == 0) addStakeholder(msg.sender);
-        stakes[msg.sender] = stakes[msg.sender].add(_stake);
+
+    // for stakeholder to add a stake working
+    function createStake(address staker, uint256 _stake) public {
+        _burn(staker, _stake);
+        if(stakes[staker] == 0) addStakeholder(staker);
+        stakes[staker] = stakes[staker].add(_stake);
     }
 
-    function removeStake(uint256 _stake) public {
-        stakes[msg.sender] = stakes[msg.sender].sub(_stake);
-        if(stakes[msg.sender] == 0) removeStakeholder(msg.sender);
-        _mint(msg.sender, _stake);
+
+    // for stakeholder to remove a stake working
+    function removeStake(address staker, uint256 _stake) public {
+        stakes[staker] = stakes[staker].sub(_stake);
+        if(stakes[staker] == 0) removeStakeholder(staker);
+        _mint(staker, _stake);
     }
+
+    // 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+    // 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+    // 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
+    // 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
+    // 0xe7150d315F8930f040Bc9d3AC40162cA78FcfaA1
+    // 0x583031D1113aD414F02576BD6afaBfb302140225
 
 
     // check stakeholder reward
@@ -115,10 +130,17 @@ contract PlynToken is ERC20, Ownable {
     }
 
 
+    // // calculate reward per stakeholder
+    // function calculateReward(address staker) public view returns(uint256) {
+    //     return stakes[staker] * 1/100;
+    // }
+
+
     // calculate reward per stakeholder
-    function calculateReward(address _stakeholder) public view returns(uint256) {
-        return stakes[_stakeholder] / 100;
+    function calculateReward(address staker) public view returns(uint256) {
+        return stakes[staker] /100;
     }
+
 
     // share rewards to stakeholders
     function distributeRewards() public onlyOwner {
